0309db1cdaf34b91b4f7b53c4d9f130ff41d5323
Jason Petterson <jp@brokenshotgun.com>
2018-12-01 14:34:20 -0500

    added day1 part 1 and part 2 solution

--- day01/src/main/kotlin/Main.kt
+++ day01/src/main/kotlin/Main.kt
@@ -3,5 +3,32 @@ import java.io.File
 fun main() {
     val input = File(ClassLoader.getSystemResource("input.txt").file)
 
-    // Write solution here!
+    var frequency = 0
+    val changeList = ArrayList<Int>()
+    input.readLines().map { line ->
+        val change = line.trim().toInt()
+        changeList.add(change)
+        frequency += change
+    }
+    println("[part1] frequency result = $frequency")
+
+    frequency = 0
+    var duplicateFreq = 0
+    var duplicateFound = false
+    val frequencySet = HashSet<Int>()
+    frequencySet.add(frequency)
+    while (!duplicateFound) {
+        changeList.forEach { change ->
+            frequency += change
+            if (!duplicateFound) {
+                if (frequencySet.contains(frequency)) {
+                    duplicateFreq = frequency
+                    duplicateFound = true
+                } else {
+                    frequencySet.add(frequency)
+                }
+            }
+        }
+    }
+    println("[part2] first duplicate = $duplicateFreq")
 }
