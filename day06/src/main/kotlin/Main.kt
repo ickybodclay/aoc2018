6190cd4ff674667167e8a3e684cffc159cce327c
Jason Petterson <jp@brokenshotgun.com>
2019-09-30 11:15:51 -0400

    added day07 part 1 solution
    also added day06 part 1 progress, not working, yet

--- day06/src/main/kotlin/Main.kt
+++ day06/src/main/kotlin/Main.kt
@@ -1,7 +1,65 @@
 import java.io.File
+import java.util.*
+import kotlin.math.abs
+import kotlin.math.tan
+
+data class Point(val x: Int, val y: Int)
 
 fun main() {
     val input = File(ClassLoader.getSystemResource("input.txt").file)
 
     // Write solution here!
+    val points = ArrayList<Point>()
+    input.readLines().map { line ->
+        val tmpStrArray = line.split(",")
+        val p = Point(tmpStrArray[0].trim().toInt(), tmpStrArray[1].trim().toInt())
+        points.add(p)
+    }
+
+    // find boundary points
+
+    // plot points on grid
+
+    // figure out manhattan distance for each point of grid, which position closest to which point, if tie ignore
+
+    // figure out point with highest count, ignoring boundary points
+}
+
+fun getManhattanDistance(a: Point, b: Point) : Int {
+    return abs(a.x - b.x) + abs(a.y - b.y)
+}
+
+// use graham scan to find boundary points
+fun getBoundaryPoints(allPoints: List<Point>) : List<Point> {
+    /*
+    let N be number of points
+    let points[N] be the array of points
+    swap points[0] with the point with the lowest y-coordinate
+    sort points by polar angle with points[0]
+    let stack = empty_stack()
+    push points[0] to stack
+    push points[1] to stack
+    for i = 2 to N-1:
+        while count stack >= 2 and ccw(next_to_top(stack), top(stack), points[i]) <= 0:
+            pop stack
+        push points[i] to stack
+    end
+     */
+    val points = ArrayList(allPoints)
+    val lowestYPoint = points.minBy { it -> it.y }
+
+
+
+    val stack = Stack<Point>()
+
+
+    return emptyList()
+}
+
+fun polarAngle(origin: Point, p: Point) : Float {
+    return tan((p.y - origin.y).toFloat() / (p.x - origin.x).toFloat())
+}
+
+fun ccw(p1: Point, p2: Point, p3: Point) : Boolean {
+    return ((p2.x - p1.x) * (p3.y - p1.y) - (p2.y - p1.y) * (p3.x - p1.x)) > 0
 }
--- day07/src/main/kotlin/Main.kt
+++ day07/src/main/kotlin/Main.kt
@@ -3,5 +3,40 @@ import java.io.File
 fun main() {
     val input = File(ClassLoader.getSystemResource("input.txt").file)
 
-    // Write solution here!
+    val stepRegex = Regex("Step ([A-Z]) must be finished before step ([A-Z]) can begin.")
+    val stepMap = mutableMapOf<String, ArrayList<String>>()
+
+    input.readLines().map {
+        val stepMatch = stepRegex.matchEntire(it)!!
+
+        val preReq = stepMatch.groups[1]!!.value
+        val step = stepMatch.groups[2]!!.value
+
+        if (!stepMap.containsKey(step)) {
+            stepMap[step] = ArrayList()
+        }
+
+        if (!stepMap.containsKey(preReq)) {
+            stepMap[preReq] = ArrayList()
+        }
+
+        stepMap[step]!!.add(preReq)
+    }
+
+    val sortedKeys = ArrayList(stepMap.keys.sorted())
+    val completedSteps = ArrayList<String>()
+
+    while (completedSteps.size != stepMap.size) {
+        for (key in sortedKeys) {
+            if (stepMap[key]!!.isEmpty() || completedSteps.containsAll(stepMap[key]!!)) {
+                sortedKeys.remove(key)
+                completedSteps.add(key)
+                break
+            }
+        }
+    }
+
+    for(step in completedSteps)
+        print(step)
+    println()
 }
