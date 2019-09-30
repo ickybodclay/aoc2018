9a055c79d261235cec3093f19f6828997b7a5fba
Jason Petterson <jp@brokenshotgun.com>
2019-09-30 15:40:50 -0400

    added day07 part 2 solution

--- day07/src/main/kotlin/Main.kt
+++ day07/src/main/kotlin/Main.kt
@@ -24,19 +24,62 @@ fun main() {
     }
 
     val sortedKeys = ArrayList(stepMap.keys.sorted())
+    val timeToCompleteMap = mutableMapOf<String, Int>()
     val completedSteps = ArrayList<String>()
 
+    for (key in sortedKeys) {
+        timeToCompleteMap[key] = timeToCompleteStep(key)
+    }
+    println(timeToCompleteMap)
+
+    var time = 0
     while (completedSteps.size != stepMap.size) {
+        val workersCount = 5
+        var workersActive = 0
+        val justCompleted = ArrayList<String>()
+
+        var workerA = "."
+        var workerB = "."
+        var workerC = "."
+        var workerD = "."
+        var workerE = "."
+
         for (key in sortedKeys) {
-            if (stepMap[key]!!.isEmpty() || completedSteps.containsAll(stepMap[key]!!)) {
-                sortedKeys.remove(key)
-                completedSteps.add(key)
-                break
+            if (workersActive < workersCount &&
+                    (stepMap[key]!!.isEmpty() || completedSteps.containsAll(stepMap[key]!!))) {
+
+                when (workersActive) {
+                    0 -> workerA = key
+                    1 -> workerB = key
+                    2 -> workerC = key
+                    3 -> workerD = key
+                    4 -> workerE = key
+                }
+
+                timeToCompleteMap[key] = timeToCompleteMap[key]!!.minus(1)
+                if (timeToCompleteMap[key]!! <= 0) {
+                    justCompleted.add(key)
+                }
+                workersActive++
             }
         }
+
+        println("${time.toString().padStart(4, '0')}\t$workerA\t$workerB\t$workerC\t$workerD\t$workerE\t$completedSteps")
+
+        sortedKeys.removeAll(justCompleted)
+        completedSteps.addAll(justCompleted)
+        time++
     }
 
-    for(step in completedSteps)
-        print(step)
-    println()
+    println(timeToCompleteMap)
+    println("total time = $time")
+
+//    for(step in completedSteps)
+//        print(step)
+//    println()
+}
+
+private val charOffset = "A"[0].toInt() // 65
+fun timeToCompleteStep(step: String) : Int {
+    return (step[0].toInt() - charOffset + 1) + 60
 }
